e51887ccda6a88de61979920459d489a2fd4956d
Steven Wen <73114262+StevenWen81@users.noreply.github.com>
2021-08-08 23:07:09 +0700

    Add files via upload

---
+++ Jawaban_Bootcamp/Jumlah_Digit.py
@@ -0,0 +1,42 @@
+'''
+Soal
+
+Hitunglah jumlah digit dari sebuah angka misalkan N
+
+(Asumsikan N bernilai positif)
+
+Test Case 1:
+Masukkan nilai N: 165
+Jumlah digit angka tersebut adalah 12
+
+Test Case 2:
+Masukkan nilai N: 2021
+Jumlah digit angka tersebut adalah 5
+
+'''
+
+# Algoritma
+# ---------
+
+# Terima angka dari pengguna
+N = int(input("Masukkan nilai N: "))
+
+# Deklarasi variabel untuk membantu penghitungan
+helper = 0
+
+# Deklarasi variabel untuk menyimpan jumlah digit
+sum_dgt = 0
+
+# Lakukan looping untuk mengecek angka satu per satu
+while ( N>0 ):
+    helper = N % 10 # Ambil digit terakhir
+    sum_dgt += helper
+    N //= 10 # Hilangkan digit terakhir
+    
+print("Jumlah digit angka tersebut adalah " + str(sum_dgt))
+'''
+Ilustrasi
+Input => 19
+19 % 10 => 9
+19 // 10 = 1
+'''
